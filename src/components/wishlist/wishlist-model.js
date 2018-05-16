af0ebccd2445423fa5223581c10fd5899d1825f9
Mat Janson Blanchet <mat.jb@jansensan.net>
2018-05-16 13:00:08 -0400

    Sorted products alphabetically

--- src/components/wishlist/wishlist-model.js
+++ src/components/wishlist/wishlist-model.js
@@ -13,7 +13,13 @@ export default class WishlistModel {
       WishlistsService
       .load(this.id)
       .then((response) => {
+        // parse json to array of objects
         this.data = JSON.parse(response);
+
+        // order array by product names
+        this.data = _.orderBy(this.data, ['name'], ['asc']);
+
+        // resolve promise
         this.isLoaded = true;
         resolve();
       });
